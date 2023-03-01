5b2189af6d5570142a1bfe2d7c9341dbb41262b2
rpassas <rpassas@mitre.org>
2023-02-28 22:13:15 -0500

    sanity check

--- src/DBConnection.ts
+++ src/DBConnection.ts
@@ -1,5 +1,42 @@
-import {Db, MongoClient} from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 var mongoUrl = "mongodb://localhost:27017/";
+import { Schema, model, connect } from 'mongoose';
+
+/**
+ * will move this to "models" and "routes" folder after a sanity check
+ */
+
+// Create an interface representing a document in MongoDB.
+interface IState {
+  name: string;
+  abbreviation: string;
+}
+
+// Create a Schema corresponding to the document interface.
+const stateSchema = new Schema<IState>({
+  name: { type: String, required: true },
+  abbreviation: { type: String, required: true },
+});
+
+// Create a Model.
+const State = model<IState>('State', stateSchema);
+
+run().catch(err => console.log(err));
+
+async function run() {
+  // Connect to MongoDB
+  await connect('mongodb://127.0.0.1:27017/test');
+
+  const state = new State({
+    name: 'Massachusetts',
+    abbreviation: 'MA'
+  });
+  await state.save();
+
+  console.log(state.abbreviation); // "MA"
+}
+
+
 
 export default class DBConnection {
 
@@ -19,21 +56,21 @@ export default class DBConnection {
    */
   public async connect(): Promise<MongoClient> {
     try {
-        if (!this.client) {
-            const dbUrl = this.mongoUrl + "/" + this.dbName;
-            console.info(`Connecting to ${dbUrl}`);
-            this.client = await MongoClient.connect(dbUrl);
-            return this.client;
-        }
-    } catch(error) {
-        console.error(error);
+      if (!this.client) {
+        const dbUrl = this.mongoUrl + "/" + this.dbName;
+        console.info(`Connecting to ${dbUrl}`);
+        this.client = await MongoClient.connect(dbUrl);
+        return this.client;
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 
   public async initializeCollections() {
     const db = this.client.db(this.dbName);
-    const collections = await db.listCollections({name: this.healthDataCollection}).toArray();
-    if(collections.length > 0) {
+    const collections = await db.listCollections({ name: this.healthDataCollection }).toArray();
+    if (collections.length > 0) {
       await db.dropCollection(this.healthDataCollection);
     }
     try {
@@ -53,15 +90,15 @@ export default class DBConnection {
   }
 
   public async getCollectionData(): Promise<string> {
-      const db = this.client.db(this.dbName);
-      console.log("db name:" + db.databaseName);
-      return db.collection(this.healthDataCollection).findOne({}).then((document) => {
-        console.log("document: " + document);
-        console.log(document._id);
-        console.log(document['title']);
-        console.log(document['content']);
-        return JSON.stringify(document);
-      });
+    const db = this.client.db(this.dbName);
+    console.log("db name:" + db.databaseName);
+    return db.collection(this.healthDataCollection).findOne({}).then((document) => {
+      console.log("document: " + document);
+      console.log(document._id);
+      console.log(document['title']);
+      console.log(document['content']);
+      return JSON.stringify(document);
+    });
   }
 
   public async closeDb() {
