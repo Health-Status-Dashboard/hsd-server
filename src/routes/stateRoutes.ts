1a38d7b12c0ac6969da2e39c0dbc194e3fc30350
kjefferson <kjefferson@mitre.org>
2023-03-13 09:43:59 -0400

    Added api/getStates

--- src/routes/stateRoutes.ts
+++ src/routes/stateRoutes.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import { initializeStates, getStates } from '../models/stateModel';
 export const stateRouter = Router();
 
-stateRouter.use('/initStates', (req, res): void => {
+stateRouter.use('/api/initStates', (req, res): void => {
     console.log("reinitializing the state collection");
     initializeStates().then(data => {
         res.send(data);
     });
 });
 
-stateRouter.use('/getStates', (req, res): void => {
+stateRouter.use('/api/getStates', (req, res): void => {
     console.log("getting the state collection");
     getStates().then(data => {
         res.send(data);
