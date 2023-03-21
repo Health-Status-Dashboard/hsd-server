1c87404d382d2edd065671c9d1d8a3302b8becdc
rpassas <rpassas@mitre.org>
2023-03-21 19:26:03 -0400

    adding route

--- src/routes/stateRoutes.ts
+++ src/routes/stateRoutes.ts
@@ -2,14 +2,13 @@ import { Router } from 'express';
 import { initializeStates, getStates } from '../models/stateModel';
 export const stateRouter = Router();
 
-// stateRouter.use('/api/initStates', (req, res): void => {
-//     console.log("A) reinitializing the state collection");
-//     initializeStates().then(data => {
-//         res.send(data);
-//         console.log(data);
-//     });
-//     console.log("B) reinitializing the state collection");
-// });
+stateRouter.use('/api/initAllStates', (req, res): void => {
+
+    initializeStates().then(data => {
+        res.send(data);
+        console.log("reinitializing the state collection");
+    });
+});
 
 stateRouter.use('/api/getAllStates', (req, res): void => {
     console.log("getting the state collection");
