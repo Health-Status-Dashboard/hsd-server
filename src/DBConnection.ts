6fd7bbc4569b5913e29e66b0068ef42fe37f6791
kjefferson <kjefferson@mitre.org>
2023-03-27 11:50:34 -0400

    took out unused/modified code from DBConnection.ts

--- src/DBConnection.ts
+++ src/DBConnection.ts
@@ -46,27 +46,6 @@ export default class DBConnection {
       await db.createCollection(this.healthDataCollection);
       const healthCollection = db.collection(this.healthDataCollection);
 
-//K
-      // const doc = {
-      //   title: "Health Data Example",
-      //   content: "This is an example of health data",
-      // }
-      // const result = await healthCollection.insertOne(doc);
-      // console.log("A document was inserted into the collection. Id: " + result.insertedId);
-
-
-      
-      //pass and continually update this list (schema above)
-      var totalHealthData = [{stateBasedData : []}, {nonStateBasedData :[]}]
-
-      //pass array of objects to each function and update with each api -- update db once below
-      totalHealthData = await(this.getLifeExpectancy(totalHealthData));
-
-      //const result = await healthCollection.insertOne(doc);
-      const secondResult = await healthCollection.insertMany(totalHealthData);
-
-      console.log(secondResult.insertedIds)
-//K
     } catch (err) {
       console.log(err);
       console.log(err.message);
@@ -76,92 +55,18 @@ export default class DBConnection {
   public async getCollectionData(): Promise<string> {
     const db = this.client.db(this.dbName);
     console.log("db name:" + db.databaseName);
-    //K
-    return db.collection(this.healthDataCollection).find().toArray().then((document) => {return JSON.stringify(document)});
-
-    // return db.collection(this.healthDataCollection).findOne({}).then((document) => {
-    //   console.log("document: " + document);
-    //   console.log(document._id);
-    //   console.log(document['title']);
-    //   console.log(document['content']);
-    //   return JSON.stringify(document);
-    // });
-    //K
+    
+    return db.collection(this.healthDataCollection).findOne({}).then((document) => {
+      console.log("document: " + document);
+      console.log(document._id);
+      console.log(document['title']);
+      console.log(document['content']);
+      return JSON.stringify(document);
+    });
   }
 
   public async closeDb() {
     this.client.close();
   }
 
-//K
-  public getLifeExpectancy = async(originalList) => {
-
-    //structure data like below 
-      // const totalHealthData = [
-      //   {stateBasedData: [{ 
-      //       state: "Virginia",
-      //       categories: [
-      //         {lifeExpectancy: {
-      //           sex: [{
-      //             total: "le",
-      //             male: "le",
-      //             female: "le"
-      //           }]
-      //         }}
-      //       ]}
-      //     },
-
-      //   {nonStateData: [{}]}
-      // ]
-
-    var listHealth = originalList;
-
-    try{
-      //get life expectancy data using cdc api
-      var life_span_api = "https://data.cdc.gov/resource/ss2j-8ajj.json";
-      var docs = await fetch(life_span_api).then(result => result.json());
-
-      //go through health data from api
-      for (var i = 0; i < docs.length; i++){
-
-        //get our array of state data
-        var arrStateData = listHealth[0].stateBasedData;
-        
-        //search for index of the state in the array if have that state already
-        var indexOfState = arrStateData.findIndex(item => item.state === docs[i].state);
-
-        var sex = docs[i].sex;
-        var lifeExpectancy = docs[i].le;
-
-        //if doesn't exist, then add value to our array
-        if (indexOfState < 0){
-          listHealth[0].stateBasedData.push(
-            {state: docs[i].state,
-            categories: [
-              {lifeExpectancy:
-                {sex: [
-                  {
-                  [sex] : lifeExpectancy
-                  }]
-                }
-              }
-            ]
-          }
-          )
-        }
-
-        //if does exist, then update with the female and male data too
-        if (indexOfState > -1){
-          var stateObjectToUpdate = listHealth[0].stateBasedData[indexOfState];
-          stateObjectToUpdate.categories[0].lifeExpectancy.sex.push({[sex] : lifeExpectancy})
-          
-        }
-        }
-       
-        return listHealth;
-        } catch(error){
-      console.log(error);
-    }
-  }
-//K
 }
